5d7a935099ce56be2b5f4522643daf428e359fd8
AVERTY PIERRE <paverty@localhost.localdomain>
2020-07-07 16:59:43 +0200

    admin

---
+++ administration/administration.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="../node_modules/trumbowyg/dist/ui/trumbowyg.min.css">
+    <title>WEA - ESPACE D'ADMINISTRATION</title>
+</head>
+
+<body class="bg-light">
+
+    <div class="container">
+        <h1 class="shadow-sm p-3 mb-5 bg-white rounded text-center mt-4">Interface d'administration de
+            WEA</h1>
+
+        <div class="hadow-sm p-3 mb-5 bg-white rounded mt-4">
+            <h1 class="d-flex justify-content-center">Paramètres Globaux</h1>
+
+            <div class="container shadow-sm p-3 mb-1 bg-light rounded mt-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Adresse Mail</span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Sizing example input"
+                        aria-describedby="inputGroup-sizing-default">
+                </div>
+            </div>
+
+            <div class="container shadow-sm p-3 mb-1 bg-light rounded mt-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Nom du site</span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Sizing example input"
+                        aria-describedby="inputGroup-sizing-default">
+                </div>
+            </div>
+
+            <div class="container shadow-sm p-3 mb-1 bg-light rounded mt-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">URL logo</span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Sizing example input"
+                        aria-describedby="inputGroup-sizing-default">
+                </div>
+            </div>
+        </div>
+
+        <div class="hadow-sm p-3 mb-5 bg-white rounded mt-4">
+        <h1 class="d-flex justify-content-center">Paramètres de la page Home</h1>
+            <div class="container shadow-sm p-3 mb-1 bg-light rounded mt-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Titre de la présentation</span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Sizing example input"
+                        aria-describedby="inputGroup-sizing-default">
+                </div>
+            </div>
+
+            <div class="container shadow-sm p-3 mb-5 bg-light rounded mt-4">
+                <h1 class="pl-2 input-group-text">Présentation du site :</h1>
+                <div id="editor">
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="../node_modules/trumbowyg/dist/trumbowyg.min.js"></script>
+    <script>
+        $(function () {
+            $('#editor').trumbowyg();
+        });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
